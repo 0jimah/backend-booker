eb288ff48f70d459512fc7823e3c3132f6127693
0jimah <godfreyakuba@gmail.com>
2023-09-19 11:50:02 +0200

    booker

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');
+  await app.listen(port);
+}
+bootstrap();
