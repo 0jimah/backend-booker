32f89d8f5831fe85bc69e1d68aa2cc39dc46c0b8
0jimah <godfreyakuba@gmail.com>
2023-09-29 16:08:29 +0200

    corsupdate

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
